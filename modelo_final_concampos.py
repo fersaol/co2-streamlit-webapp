63a2af2b803f90d622034e0fbac247c630c50862
fersaol <fersaolade@gmail.com>
2024-03-20 21:27:21 +0000

    trying markdown

--- modelo_final_concampos.py
+++ modelo_final_concampos.py
@@ -358,13 +358,13 @@ class Final_Model:
             elif tag == 2:
                 flag= f"""The efficiency predicted for your country is {efi}, 
     what means it is classified in the environmental group {tag}.
-    This group is characterized by the following description:
+    This group is characterized by the following description:  
 
     ------VERY HIGH PRODUCTION-VERY HIGH CONTAMINATION------
     The energy production is very high and contamination too, so
     you are one of the world's major suppliers. The production in
     this group comes normally from petroleum, coal and natural
-    gas
+    gas  
 
     -----------------------RECOMMENDATION---------------------
     Your country has great impact on environmental care so it would
